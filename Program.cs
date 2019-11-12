3928b5efc23218b3c1a4ce157a7286fc1b8e27e1
Stef van Stipdonk <mrbats1234@gmail.com>
2019-11-12 11:08:52 +0100

    Added all the needed methods

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace Coin_change
 {
     class Program
     {
+        static void ChangeCalc(double enterAmount, double amountLeft, int[] coins)
+{
+    
+}
         static void Main(string[] args)
         {
             
